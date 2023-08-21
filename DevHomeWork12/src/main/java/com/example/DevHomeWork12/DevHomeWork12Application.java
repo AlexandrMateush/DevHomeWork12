9063ab33387c6d0c9edb197fefce3f09b2dd8656
AlexandrMateush <128997943+AlexandrMateush@users.noreply.github.com>
2023-08-21 20:55:12 +0300

    Add files via upload

---
+++ DevHomeWork12/src/main/java/com/example/DevHomeWork12/DevHomeWork12Application.java
@@ -0,0 +1,13 @@
+package com.example.DevHomeWork12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevHomeWork12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevHomeWork12Application.class, args);
+	}
+
+}
---
+++ DevHomeWork12/src/test/java/com/example/DevHomeWork12/DevHomeWork12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DevHomeWork12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevHomeWork12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
